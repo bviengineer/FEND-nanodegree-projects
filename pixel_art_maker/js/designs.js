6ebc9ad446ec009fcba9086fed6f836632a6256c
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-05-31 14:13:30 -0400

    temporarily hard coded grid using html & js to test theory

--- pixel_art_maker/js/designs.js
+++ pixel_art_maker/js/designs.js
@@ -2,8 +2,14 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+document.body.addEventListener("click", function(){
+  const newTable = document.createElement("tr");
+  newTable.innerHTML = "<td>used js-row 1, cell 1</td><td>r1c2</td><td>r1c3</td><td>r1c4</td><td>r1c5</td>";
+  document.getElementById("pixelCanvas").appendChild(newTable);
+});
 
 function makeGrid() {
     // Your code goes here!
+
   }
   
