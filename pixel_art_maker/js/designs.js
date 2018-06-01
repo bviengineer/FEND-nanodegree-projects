5dfb4e5bbfb796ace7da7e4c4bd7ffd20368aa2e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-06-01 02:43:08 -0400

    Click anywhere on document will create a table grid using predefined selections for rows & heights

--- pixel_art_maker/js/designs.js
+++ pixel_art_maker/js/designs.js
@@ -1,15 +1,25 @@
 // Select color input
 // Select size input
+let myCanvas = document.getElementById("pixelCanvas");
+const noOfRows = 10;
+const noOfCols = 20;
 
 // When size is submitted by the user, call makeGrid()
 document.body.addEventListener("click", function(){
-  const newTable = document.createElement("tr");
-  newTable.innerHTML = "<td>used js-row 1, cell 1</td><td>r1c2</td><td>r1c3</td><td>r1c4</td><td>r1c5</td>";
-  document.getElementById("pixelCanvas").appendChild(newTable);
+  makeGrid();
 });
 
 function makeGrid() {
-    // Your code goes here!
-
+  // Your code goes here!
+    let canvasRow = 0;
+    let canvasCol = 0;
+    
+  for (let rows = 0; rows < noOfRows; rows++) {
+    canvasRow = myCanvas.insertRow(rows);
+    
+    for (let cols = 0; cols < noOfCols; cols++) {
+      canvasCol = canvasRow.insertCell(cols);
+    }    
   }
+} 
   
