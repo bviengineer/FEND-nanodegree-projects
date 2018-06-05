e0ebe8328c0c7ad1ec86c0711967601e798a4c2c
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-06-05 18:44:15 -0400

    Rename variabl canvasCol to canvasCell and declare canvasRow and CanvasCell globally

--- pixel_art_maker/js/designs.js
+++ pixel_art_maker/js/designs.js
@@ -5,40 +5,32 @@ const color = document.getElementById("colorPicker"),
       canvasWidth = document.getElementById("inputWidth");
 
 let submitButton = document.getElementById("submit"),
-    myCanvas = document.getElementById("pixelCanvas");
-  
+    myCanvas = document.getElementById("pixelCanvas"),
+    canvasRow = "",
+    canvasCell = "",
+    colorCell = "";
     
 // FUNCTION(S) & LISTENERS 
-submitButton.addEventListener("click", function(){
-  
-  myCanvas.innerHTML = ""; //clears table rows & cells in the event of a new grid size submission request
-
-  makeGrid(); //calls makeGrid function
-  
+submitButton.addEventListener("click", function(){  
+  //clears table rows & cells in the event of a new grid size submission request
+  myCanvas.innerHTML = ""; 
+  makeGrid();
   //will prevent form from refreshing the page and thus removing the grid immeditely after it has been displayed
   form.addEventListener("click", function(event) {
     event.preventDefault(); 
-  }); //END form addEventListener
-}); //END submitButton addEventListener
+  });
+});
 
-function makeGrid(){
+function makeGrid(event){
   for (let rows = 0; rows < canvasHeight.value; rows++) {
-    let canvasRow = myCanvas.insertRow(rows);
-    for (let cols = 0; cols < canvasWidth.value; cols++) {
-      let canvasCol = canvasRow.insertCell(cols);
-    } //END canvasWidth
-  } //END canvasHeight
-} // END makeGrid()
+    canvasRow = myCanvas.insertRow(rows);
+    for (let cells = 0; cells < canvasWidth.value; cells++) {
+      canvasCell = canvasRow.insertCell(cells);
+    } 
+  } 
+} 
 
 // Color individual cells
 myCanvas.addEventListener("click", function(event){
   event.target.style.backgroundColor = color.value;
-}); //END myCanvas addEventListener
-
-// END SCRIPT
-
-/*
-PROUD MOMENT: These two lines of statements were instrumental in developing the code that targets individual cells to color and remains noted here as a personal reminder.  
-  console.log(myCanvas.children[0].childNodes); 
-  console.log(event); 
-*/
+}); 
