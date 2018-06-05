e9dfd2a33d0d6aee83dbb25c96ed004aa2f95c69
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-06-04 22:51:31 -0400

    Remove rows and cells from table in the event of a new grid size request submission

--- pixel_art_maker/js/designs.js
+++ pixel_art_maker/js/designs.js
@@ -1,4 +1,4 @@
-// SELECTIONS //
+// SELECTIONS 
 const color = document.getElementById("colorPicker"),
       form = document.getElementById("sizePicker"),
       canvasHeight = document.getElementById("inputHeight"),
@@ -8,10 +8,14 @@ let submitButton = document.getElementById("submit"),
     myCanvas = document.getElementById("pixelCanvas");
   
     
-// FUNCTION(S) & LISTENERS //
+// FUNCTION(S) & LISTENERS 
 submitButton.addEventListener("click", function(){
-  makeGrid();
   
+  myCanvas.innerHTML = ""; //clears table rows & cells in the event of a new grid size submission request
+
+  makeGrid(); //calls makeGrid function
+  
+  //will prevent form from refreshing the page and thus removing the grid immeditely after it has been displayed
   form.addEventListener("click", function(event) {
     event.preventDefault(); 
   }); //END form addEventListener
@@ -29,13 +33,12 @@ function makeGrid(){
 // Color individual cells
 myCanvas.addEventListener("click", function(event){
   event.target.style.backgroundColor = color.value;
-});//END myCanvas addEventListener
+}); //END myCanvas addEventListener
 
 // END SCRIPT
 
 /*
-
-PROUD MOMENT: These two lines of statements were instrumental in developing the code above that targets individual cells to color and remains noted here as a personal reminder.  
-          console.log(myCanvas.children[0].childNodes); 
-          console.log(event); 
+PROUD MOMENT: These two lines of statements were instrumental in developing the code that targets individual cells to color and remains noted here as a personal reminder.  
+  console.log(myCanvas.children[0].childNodes); 
+  console.log(event); 
 */
