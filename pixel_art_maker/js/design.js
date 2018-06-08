9dd0e3a1afd7d70bcebc7c70d16fabcaa8c77645
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-06-08 17:20:59 -0400

    Implement new js file with updated solution(s)

---
+++ pixel_art_maker/js/design.js
@@ -0,0 +1,49 @@
+// SELECTIONS 
+const color = document.getElementById("colorPicker"),
+      form = document.getElementById("sizePicker"),
+      canvasHeight = document.getElementById("inputHeight"),
+      canvasWidth = document.getElementById("inputWidth");
+
+let submitButton = document.getElementById("submit"),
+    myCanvas = document.getElementById("pixelCanvas"),
+    canvasRow = "",
+    canvasCell = "",
+    colorCell = "";
+    
+// FUNCTION(S) & LISTENERS 
+submitButton.addEventListener("click", function(){  
+  //clears table rows & cells in the event of a new grid size submission request
+  myCanvas.innerHTML = ""; 
+  makeGrid();
+  //will prevent form from refreshing the page and thus removing the grid immeditely after it has been displayed
+  form.addEventListener("click", function(event) {
+    event.preventDefault(); 
+  });
+});
+
+function makeGrid(event){
+  for (let rows = 0; rows < canvasHeight.value; rows++) {
+    canvasRow = myCanvas.insertRow(rows);
+    for (let cells = 0; cells < canvasWidth.value; cells++) {
+      canvasCell = canvasRow.insertCell(cells);
+
+      //2nd solution
+      // canvasCell.addEventListener("click", function(event){
+      //   event.target.style.backgroundColor = color.value;
+      // });
+
+      //3rd solution - step 2
+      canvasCell.addEventListener("click", clickEventListener);
+    }  
+  } 
+} 
+
+//3rd solution - step 1 
+var clickEventListener = function (event){
+  event.target.style.backgroundColor = color.value;
+}; 
+
+// Color individual cells - INITIAL solution
+// myCanvas.addEventListener("click", function(event){
+//   event.target.style.backgroundColor = color.value;
+// }); 
